4db133eb492f96b8cad6ac0e2821066bd3f20cfc
Allef Gomes <allefgalmeida@gmail.com>
2024-08-19 23:41:46 -0300

    feat: config styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,25 +8,24 @@ module.exports = {
   ],
   theme: {
     extend: {
-     colors: {
-        'elixir' : {
-          DEFAULT: '#6E4C6C',
-          light: '#8F6D8D',
-          lighter: '#B08EAE',
-          lightest: '#D1B0CF',
-          dark: '#4D3B4B',
-          darker: '#2C222A',
-          darkest: '#0B0909',
+      colors: {
+        'elixir': {
+          DEFAULT: '#270640',
         },
-        'muted-green': '#4C6E4E',
-        'muted-red': '#6E4C4C',
-        'muted-blue': '#4C4C6E',
-        'neutral': {
-          light: '#F0F0F0',
-          medium: '#C0C0C0',
-          dark: '#404040',
+        'orange': '#ff7849',
+      },
+      container: {
+        center: true,
+        padding: '1rem',
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1280px',
+          '2xl': '1536px',
         },
-      },    },
+      },
+    },
   },
   plugins: [],
 }
